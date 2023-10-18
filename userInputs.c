04c0c9f20ea77c549fc55ec6e5e009791727e549
Dilsin Amirtharajah <59810782+Dilsinnam@users.noreply.github.com>
2023-10-18 00:34:03 -0400

    Updated variables

--- userInputs.c
+++ userInputs.c
@@ -1,11 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    int player;
-    printf("Who is the best football player in the world?");              /*prompt the user*/
-    scanf("%c", &player);                                                 /*allows for user input, getting the input as a word. Then, store input in declared variable. Need the &*/
-    printf("You are right, %c is the best player in the world!", player); /*printing user input*/
-    return 0;
+int main() {
+  int age;
+  printf("What is the best number in the world?"); /*prompt the user*/
+  scanf("%d", &age); /*allows for user input, getting the input as a word. Then,
+                        store input in declared variable. Need the &*/
+  printf("You are right, %d is the best number in the world!",
+         age); /*printing user input*/
+  return 0;
 }
